37dbb0b9988d6e8ef8ee7a92b6be266b642e04d8
motiullahsajit <sajitmotiullah@gmail.com>
2023-02-16 13:52:45 +0600

    minor update

--- src/components/Team/Team.js
+++ src/components/Team/Team.js
@@ -26,7 +26,7 @@ function Team() {
                         <img src ={tamim}/>
                         <a href='https://www.linkedin.com/in/md-tamim-hossain-61a092208/' target="_blank" ><h3 className='primary-color'>Md Tamim Hossain</h3></a>
                         <h5 className='primary-color'>(Course Instructor)</h5>
-                        <p>Studying Computing and Information System, Connected with programming around for 4 years now, Experienced in Comparative Programming, CyberSecurity and back-end development</p>
+                        <p>Studying Computing and Information System, Connected with programming around for 5 years now, 1 year experience in Teaching, Experienced in Comparative Programming, CyberSecurity and Back-end development</p>
                     </div>
                 </div>
                 <div className='team-card-container'>
@@ -46,7 +46,7 @@ function Team() {
                         <img src ={smaron}/>
                         <a href='https://www.facebook.com/profile.php?id=100011547033268' target="_blank" ><h3 className='primary-color'>Smaron Biswas</h3></a>
                         <h5 className='primary-color'>(Executive)</h5>
-                        <p>Studying Computing and Information System, Connected with programming around for 4 years now, Experienced in Comparative Programming, back-end development</p>
+                        <p>Studying Computing and Information System, Connected with programming around for 4 years now, Experienced in Comparative Programming, Back-end development</p>
                     </div>
                 </div>
             </div>
