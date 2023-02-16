2daa3ccaa4bea6a4a0c3ac90feb0d0c2b8fdf2c5
motiullahsajit <sajitmotiullah@gmail.com>
2023-02-16 15:43:48 +0600

    linkedin added

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -20,7 +20,7 @@ function Footer() {
                 <h3 className="footer-top-head primary-color">Social Media Links</h3>
                 <p><FacebookIcon className='facebook m-r-1'/> <a target="_blank" href='https://www.facebook.com/codemarsteam'>Facebook Page</a></p>
                 <p><FacebookIcon className='facebook m-r-1'/> <a target="_blank" href='https://www.facebook.com/groups/1215593719056089/'>Facebook Group</a></p>
-                <p><LinkedInIcon className='linkedin m-r-1'/> <a target="_blank" href='https://www.facebook.com/codemarsteam'>LinkedIn</a></p>
+                <p><LinkedInIcon className='linkedin m-r-1'/> <a target="_blank" href='https://www.linkedin.com/company/teamcodemars/'>LinkedIn</a></p>
                 <p><YouTubeIcon className='youtube m-r-1'/> <a target="_blank" href='https://www.youtube.com/channel/UCW83TqGKPvLSUGRrYDdJ8Mw'>Youtube</a></p>
                 </div>
             </div>
