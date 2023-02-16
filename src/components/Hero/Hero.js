3ef7ec0897c011a90f54850210c18f71a84b95ae
motiullahsajit <sajitmotiullah@gmail.com>
2023-02-17 00:34:39 +0600

    iframe added

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,10 +1,9 @@
 import React from 'react'
-import coverPhoto from '../../images/cover.png'
 import "./hero.css"
 
 function Hero() {
   return (
-    <section style={{display: "flex", justifyContent: "center",marginTop: "6rem"}}>
+    <section style={{display: "flex", justifyContent: "center",marginTop: "8rem"}}>
         <div className='hero-container'>
         <div className='left-container'>
             <h1><span className='secondary-color'>Star Your<br/> Journey to Become<br/> a </span><span className='primary-color'>Programmer!</span></h1>
@@ -12,11 +11,11 @@ function Hero() {
             <p>
             শূন্য থেকে শুরু করে (C) Programming এর basic শিখে, beginner level Problem-solving করে একজন দক্ষ প্রোগ্রামার হওয়ার যাত্রা শুর করতে চাইলে এই কোর্সে জয়েন করো |
             </p>
-            <br />
+            <br /><br />
             <a href='https://forms.gle/yK2zvZNcAtTDkMDr9' className='primary-button'>Enroll Now</a>
         </div>
         <div className='right-container'>
-            <img src={coverPhoto}/>
+            <iframe height="315" src="https://www.youtube.com/embed/CMvQCoh2q9U" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
         </div>
         </div>
     </section>
